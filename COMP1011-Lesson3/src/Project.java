78458debd77d40a8f5c5467f21307fe0ac47c56e
chutch01 <chutch01@hotmail.com>
2015-01-22 15:42:49 -0500

    added commenting

--- COMP1011-Lesson3/src/Project.java
+++ COMP1011-Lesson3/src/Project.java
@@ -4,11 +4,14 @@
 
 /**
  * @author chutc_000
+ * COMP1011 Lesson 03
+ * @version 0.1 - Created Basic Project
  *
  */
 public class Project {
 
 	/**
+	 * main method for projects
 	 * @param args
 	 */
 	public static void main(String[] args) {
